951c718b0d96b6b9725578fad15952a50d92361c
Kevin Okoth <kevinokotth04@gmail.com>
2023-01-30 19:40:03 +0300

    Modify html,css and js files

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
+// select elements
+const menu = document.querySelector('.menu');
+const menuItems = document.querySelector('.menuItem');
+const hamburger = document.querySelector('.hamburger');
+const closeIcon = document.querySelector('.closeIcon');
+const menuIcon = document.querySelector('.menuIcon');
 
-//select elements
-const menu=document.querySelector('.menu');
-const menuItems=document.querySelector('.menuItem');
-const hamburger=document.querySelector('.hamburger');
-const closeIcon=document.querySelector('.closeIcon');
-const menuIcon=document.querySelector('.menuIcon');
-//condition
+// condition
 function toggleMenu() {
-    if(menu.classList.contains("showmenu")) {
-        menu.classList.remove("showmenu");
-        closeIcon.style.display="none";
-        menuIcon.style.display="block";
-    } else {
-       menu.classList.add("showmenu");
-       closeIcon.style.display="block";
-       menuIcon.style.display="none"; 
-    }
+  if (menu.classList.contains('showMenu')) {
+    menu.classList.remove('showMenu');
+    closeIcon.style.display = 'none';
+    menuIcon.style.display = 'block';
+  } else {
+    menu.classList.add('showMenu');
+    closeIcon.style.display = 'block';
+    menuIcon.style.display = 'none';
+  }
 }
-hamburger.addEventListener("click",toggleMenu);
-//hide menu if user clicks on the menu links
-menuItems.forEach(function(menuItem) {
-    menuItem.addEventListener("click",toggleMenu);
-}
-)
- 
+hamburger.addEventListener('click', toggleMenu);
+// hide menu if user clicks on the menu links
+menuItems.forEach((menuItem) => {
+  menuItem.addEventListener('click', toggleMenu);
+});
