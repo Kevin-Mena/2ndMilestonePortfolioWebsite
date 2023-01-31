ead571760773c903711da911bc3ff29543200770
Kevin Okoth <kevinokotth04@gmail.com>
2023-01-31 11:38:28 +0300

    Modify html/css and js files

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // select elements
-const menu = document.querySelector('.menu');
+const menu = document.querySelector('.mobile-menu');
 const menuItems = document.querySelector('.menuItem');
 const hamburger = document.querySelector('.hamburger');
 const closeIcon = document.querySelector('.closeIcon');
 const menuIcon = document.querySelector('.menuIcon');
+const headerSection = document.querySelector('.div-header');
 
 // condition
 function toggleMenu() {
@@ -11,14 +12,28 @@ function toggleMenu() {
     menu.classList.remove('showMenu');
     closeIcon.style.display = 'none';
     menuIcon.style.display = 'block';
+    headerSection.style.filter = 'blur(0)';
   } else {
     menu.classList.add('showMenu');
     closeIcon.style.display = 'block';
     menuIcon.style.display = 'none';
+    headerSection.style.filter = 'blur(5px)';
   }
 }
+
 hamburger.addEventListener('click', toggleMenu);
 // hide menu if user clicks on the menu links
-menuItems.forEach((menuItem) => {
+/* menuItems.forEach((menuItem) => {
   menuItem.addEventListener('click', toggleMenu);
+}); */
+
+function toggleMenuItem() {
+  if (menu.classList.contains('showMenu')) {
+    menu.classList.remove('showMenu');
+    closeIcon.style.display = 'none';
+    menuIcon.style.display = 'block';
+  }
+}
+menuItems.forEach((menuItem) => {
+  menuItem.addEventListener('click', toggleMenuItem);
 });
