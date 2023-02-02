ef2f3bcf8530267a71be9cf817f393b41c7a5fba
Kevin Okoth <kevinokotth04@gmail.com>
2023-02-02 18:21:35 +0300

    Update script.js and fix eslint errors

--- script.js
+++ script.js
@@ -9,6 +9,22 @@ document.querySelectorAll('.navlink').forEach((n) => n.addEventListener('click',
   navMenu.classList.remove('show');
 }));
 
+// form validation
+const form = document.querySelector('form');
+const email = document.querySelector('#email');
+const errorMessage = document.querySelector('#errMsg');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const emailLowerCase = email.value;
+  if (emailLowerCase.toLowerCase() === emailLowerCase) {
+    form.submit();
+  } else {
+    errorMessage.textContent = 'Please enter email in lowercase!';
+    errorMessage.style.color = '#ff0000';
+  }
+});
+
 // create object array
 const array = [
   {
@@ -147,6 +163,7 @@ function display(main, i) {
   displayPopup.innerHTML = popup;
   extra.appendChild(displayPopup);
 }
+
 project.forEach((pro, index) => {
   pro.addEventListener('click', () => {
     const v = index;
