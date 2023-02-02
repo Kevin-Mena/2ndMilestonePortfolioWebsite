dcc4f5b5863b191a129aada705809c5087c32b49
Kevin Okoth <kevinokotth04@gmail.com>
2023-02-02 15:59:19 +0300

    Add form validation

--- script.js
+++ script.js
@@ -162,3 +162,20 @@ project.forEach((pro, index) => {
     }
   });
 });
+
+// form validation
+const form = document.querySelector('form');
+const email = document.querySelector('#email');
+const errorMessage = document.querySelector('#errMsg');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const emailLowerCase = email.value;
+  if (emailLowerCase.toLowerCase() === emailLowerCase) {
+    form.submit();
+  } else {
+    errorMessage.textContent = 'Please enter email in lowercase!';
+    errorMessage.style.color = '#ff0000';
+  }
+});
